9582e2ddac626cc51d4ea97f54c013b07e45dd72
BrownCarter <lilbrown3000@gmail.com>
2023-06-04 14:10:39 +0100

    feat: script to change border color

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello from Nerdbord!')
+let box = document.querySelector(".box");
+
+const color = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+const getRandom = () => Math.floor(Math.random() * color.length);
+
+console.log(getRandom());
+setInterval(() => {
+  box.style.borderColor = color[getRandom()];
+}, 3000)
